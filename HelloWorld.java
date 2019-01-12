48a10842f655e9cda37e458407af2393648d0a8a
Pavan Kumar Gajavalli <gpkucm@gmail.com>
2019-01-12 17:29:28 -0600

    commit helloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+public static void main(String[] arg) {
+	int i;
+	for (i=1; i<5;i++) {
+	System.out.println("Hello World");
+	}
+ }
+}
---
+++ net/codejava/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<=6;i++) {
+		System.out.println("Hello World");
+		}
+		}
+
+}
